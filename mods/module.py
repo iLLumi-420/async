06e170dad8fb411bec11647dfa720a756a61a577
iLLumi-420 <sandeepparajuli96@gmail.com>
2023-07-27 13:58:44 +0545

    error handling

--- mods/module.py
+++ mods/module.py
@@ -8,23 +8,39 @@ import time
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey=GZU5N3ZE942VPFBM'
 symbols = ['AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT',]
 
-results = []
+results = {}
 
 start = time.time()
 
 def get_tasks(session):
-    tasks = []
+    tasks = {}
     for symbol in symbols:
-        tasks.append(session.get(url.format(symbol)))
+        tasks[symbol] = asyncio.create_task(session.get(url.format(symbol)))
     return tasks
 
 async def get_data():
     async with aiohttp.ClientSession() as session:
         tasks = get_tasks(session)
-        responses = await asyncio.gather(*tasks)
 
-        for response in responses:
-            results.append(await response.json())
+        completed, pending = await asyncio.wait(tasks.values())
+
+        for symbol, task in tasks.items():
+            if task in completed:
+                try:
+                    response = await task
+                    result = await response.json()
+                    results[symbol] = result
+
+                except Exception as e:
+                    print(f'Error for symbol {symbol}: {e}')
+
+            else:
+                print(f'Task for symbol {symbol} did not finish')
+        
+        for task in pending:
+            task.cancel()
+        
+
 
 
 asyncio.run(get_data())
@@ -33,5 +49,7 @@ end = time.time()
 total = end-start
 
 print(f'It took {total} time to get the results')
+
+
 
 
